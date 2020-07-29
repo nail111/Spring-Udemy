843b2d82852a6e3009671bd34dbb5e7e87464e47
nail111 <g.alpha@mail.ru>
2020-07-29 17:11:00 +0400

    Annotation: Component

--- src/main/java/Step8_AnnotationsBeginning/Application.java
+++ src/main/java/Step8_AnnotationsBeginning/Application.java
@@ -3,12 +3,16 @@ package Step8_AnnotationsBeginning;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Component;
 
+
+
 @Component("myCoach")
 class FootballCoach {
     public String getDailyWorkout() {
         return "Football";
     }
 }
+
+
 public class Application {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("Step8\\ApplicationContext.xml");
